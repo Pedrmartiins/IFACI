670853fe1766b8c9c37980bd203e1fbfff2de5e4
pedrMartiins <pedroenriquellopes2011@gmail.com>
2025-03-26 21:55:44 -0300

    Atualizacao aula 6

---
+++ Aula 6/COMUNICAÇÃO_SERIAL/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace COMUNICAÇÃO_SERIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ligar = new System.Windows.Forms.Button();
+            this.ConectarBt = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Ligar
+            // 
+            this.Ligar.Location = new System.Drawing.Point(197, 285);
+            this.Ligar.Name = "Ligar";
+            this.Ligar.Size = new System.Drawing.Size(75, 23);
+            this.Ligar.TabIndex = 0;
+            this.Ligar.Text = "Ligar";
+            this.Ligar.UseVisualStyleBackColor = true;
+            this.Ligar.Click += new System.EventHandler(this.Ligar_Click);
+            // 
+            // ConectarBt
+            // 
+            this.ConectarBt.Location = new System.Drawing.Point(62, 36);
+            this.ConectarBt.Name = "ConectarBt";
+            this.ConectarBt.Size = new System.Drawing.Size(140, 47);
+            this.ConectarBt.TabIndex = 1;
+            this.ConectarBt.Text = "Conectar";
+            this.ConectarBt.UseVisualStyleBackColor = true;
+            this.ConectarBt.Click += new System.EventHandler(this.ConectarBt_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(242, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Enabled = true;
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.timerCOM_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LED";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 422);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ConectarBt);
+            this.Controls.Add(this.Ligar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ligar;
+        private System.Windows.Forms.Button ConectarBt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.Windows.Forms.Timer timer2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Aula 6/COMUNICAÇÃO_SERIAL/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMUNICAÇÃO_SERIAL
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder BufferedRecebido = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+            timerCOM.Interval = 2000;
+            timerCOM.Start();
+
+        }
+
+        private void Ligar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    if (Ligar.Text == "Ligar")
+                    {
+                        serialPort1.Write("L\n");
+                        Ligar.Text = "Desligar";
+
+                    }
+                    else
+                    {
+                        serialPort1.Write("D\n");
+                        Ligar.Text = "Ligar";
+
+                    }
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Erro ao enviar comando: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+        private void atualizarListaCOMs()
+        {
+            comboBox1.Items.Clear();
+            string[] portas = serialPort1.GetPortNames();
+
+            if (portas.Length == 0)
+            {
+                comboBox1.Items.Add("Nenhuma porta encontrada");
+                comboBox1.SelectedIndex = 0;
+                return;
+            }
+            foreach (string porta in portas)
+            {
+                comboBox1.Items.Add(porta);
+                comboBox1.SelectedIndex = 0;
+            }
+          
+        }
+
+        private void timerCOM_Tick(object sender, EventArgs e)
+        {
+            atualizarListaCOMs();
+        }
+
+        private void ConectarBt_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                desconectarSerial();
+
+            }
+            else
+            {
+                conectarSerial();
+            }
+            
+        }
+        private void conectarSerial()
+        {
+            if(comboBox1.SelectedIndex == null || comboBox1.SelectedItem.ToString() == "Nenhuma porta encontrada") {
+                MessageBox.Show("Selecione uma porta COM valida!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                serialPort1.Handshake = System.IO.Ports.Handshake.None;
+
+
+                serialPort1.Open();
+
+                ConectarBt.Text = "Desconectar";
+                comboBox1.Enabled = false;
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Erro ao conectar: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        private void desconectarSerial()
+        {
+            try
+            {
+                serialPort1.Close();
+                comboBox1.Enabled = true;
+                ConectarBt.Text = "conectar";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao desconectar: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+             serialPort1.Close();
+
+        }
+       
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+        }
+    }
